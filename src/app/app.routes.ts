ab907693995f58748858fb395ac8f3ae12db2ed8
Mahmood Ajawi <mahmoud@metyaf.com>
2024-09-16 20:01:57 +0300

    add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { EmployeeDetailComponent } from './components/employee-detail/employee-detail.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { EmployeeFormComponent } from './components/employee-form/employee-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'employees', component: EmployeeListComponent },
+    { path: 'employees/:id', component: EmployeeDetailComponent },
+    { path: 'employees/create', component: EmployeeFormComponent },
+    { path: '**', redirectTo: '' } 
+  ];
